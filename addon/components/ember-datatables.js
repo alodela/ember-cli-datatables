cf9e43e11e1e89083c78ea4fd654f60084592e74
Alberto López de Lara <alberto@agilekoding.com>
2016-02-18 22:59:25 -0600

    Autofocus & 'enter' event when user press enter key

--- addon/components/ember-datatables.js
+++ addon/components/ember-datatables.js
@@ -5,22 +5,23 @@ export default Ember.Component.extend(Ember.Evented, {
   layout: layout,
   tagName: 'table',
   classNames: ['table', 'table-striped', 'table-bordered'],
-	attributeBindings: ['tabIndex'],
+  attributeBindings: ['tabIndex'],
 
-	table: null,
+  table: null,
   fixedHeader: null,
   columns: [],  
-	cells: [],
+  cells: [],
   searching: false,
   ordering: false,
   data: null,
   scrollY: false,
   autoWidth: true,
   headerOffset: 0,
-	paging: false,
+  paging: false,
+  autofocus: false,
 
-	dataTablesColumnDefs: Ember.A(),
-	dataTablesColumns: Ember.K,
+  dataTablesColumnDefs: Ember.A(),
+  dataTablesColumns: Ember.K,
 
   // _resolvedContent is an intermediate property between content and rows
   // This allows content to be a plain array or a promise resolving to an array
@@ -55,19 +56,27 @@ export default Ember.Component.extend(Ember.Evented, {
   }),
 
   resolvedDataChanged: Ember.observer('_resolvedData.[]', function() {
-    Ember.run.once(this, 'redraw');
+    // Ember.run.once(this, 'redraw');
+    Ember.run.scheduleOnce('afterRender', this, 'redraw');
   }),
 
   redraw: function() {
     var scrollPos = Ember.$(window).scrollTop();
-    var header = this.get('table').fixedHeader;
+    var table = this.get('table')
+    var header = table.fixedHeader;
 
-		this.removeAllChildren(); 
+    this.removeAllChildren(); 
     this.$().DataTable().draw();
+
+    var _this = this;
 
     setTimeout(function() {
       Ember.$(window).scrollTop(scrollPos);
       header.adjust();
+      _this.selectRow(_this.$().find('tbody tr:first-child'));
+      if (_this.get('autofocus')) {
+        table.cell(':eq(0)').focus();
+      }
     }, 500);
   },
 
@@ -91,42 +100,39 @@ export default Ember.Component.extend(Ember.Evented, {
     };
   },
 
-	willInsertElement: function() {
+  willInsertElement: function() {
     var columns = this.get('columns') || Ember.A();
     var columnsDefinition = Ember.A();
     var columnDefs = Ember.A();
 
-		this.set('cells', Ember.A());
-		var _this = this;
+    this.set('cells', Ember.A());
+    var _this = this;
     
     columns.forEach(function(col, idx) {
       if (typeof col.getColumnDefinition === 'function') {
-				columnsDefinition.push(col.getColumnDefinition());
+        columnsDefinition.push(col.getColumnDefinition());
 
- 				columnDefs.push({
-					targets: idx,
-					createdCell: function( cell, cellData, rowData ) {
-						var cellClass = _this.container.lookupFactory("component:" + col.get('tableCellViewClass'));
-						if (col.get('template')) {
-            	cellClass = cellClass.extend({template: col.get('template')});
-						}
-            var view = cellClass.create({ row: rowData, column: col, parentView: _this });
-            _this.childViews.pushObject(view);
-						view.appendTo(_this.$(cell));
-						return "";
-					},	
-      		data: function () {
-						return "";
-					}
-				});
+        columnDefs.push({
+          targets: idx,
+          createdCell: function( cell, cellData, rowData ) {
+            var view = col.getCellView(_this, rowData);
+            if (view) {
+              view.appendTo(_this.$(cell));
+            }
+            return "";
+          },  
+          data: function (row) {
+            return col.getCellContent(row);
+          }
+        });
       } else {
         columnsDefinition.push(col);
       }
     });
 
-		this.set('dataTablesColumns', columnsDefinition);
-		this.set('dataTablesColumnDefs', columnDefs);
-	},
+    this.set('dataTablesColumns', columnsDefinition);
+    this.set('dataTablesColumnDefs', columnDefs);
+  },
 
   didInsertElement: function() {
     var columnsDefinition = this.get('dataTablesColumns');
@@ -151,70 +157,76 @@ export default Ember.Component.extend(Ember.Evented, {
       paging: this.get('paging'),
       searching: this.get('searching'),
       serverSide: true,
-			scrollY: this.get('scrollY'),
-			deferRender: true,
-			keys: true,
+      scrollY: this.get('scrollY'),
+      deferRender: true,
+      keys: true,
       language: {
         emptyTable: "No hay información disponible"
       }
     });
 
-		var _this = this;
+    var _this = this;
 
-		table
-			.on('key-focus', function(e, datatable, cell) {
-				_this.selectRow(Ember.$(datatable.row(cell.index().row).node()));
-			});
+    table
+      .on('key-focus', function(e, datatable, cell) {
+        _this.selectRow(Ember.$(datatable.row(cell.index().row).node()));
+      })
+      .on('key', function(e, datatable, key, cell, originalEvent) {
+        _this.move(key);
+      })
 
 
     this.$().on('click', 'tbody tr', function(evt) {
       _this.selectRow(Ember.$(evt.currentTarget));
     });
 
-		this.set('table', table);
+    this.set('table', table);
   },
 
-	willDestroyElement: function() {
+  willDestroyElement: function() {
     if (this.get('notifier')) {
       this.get('notifier').off('resize', this, 'onResize');
     }
-		this.get('table').destroy();
-	},
+    this.get('table').destroy();
+  },
 
   prevRow: function() {
     this.move('up');
-	},
+  },
 
   nextRow: function() {
     this.move('down');
-	},
+  },
 
   move: function(key) {
     var selected      = this.getSelectedRow();
     var tbody         = this.$().find('tbody');
-		var row						= null;
+    var row           = null;
 
     switch(key) {
       case 'up':
         row = selected.prev();
-				if (row.length === 0) {
-        	row = tbody.find('tr:last-child');
-				}
+        if (row.length === 0) {
+          row = tbody.find('tr:last-child');
+        }
         this.selectRow(row);
         break;
       case 'down':
         row = selected.next();
-				if (row.length === 0) {
-	        row = tbody.find('tr:first-child');
-				}
-       	this.selectRow(row);
+        if (row.length === 0) {
+          row = tbody.find('tr:first-child');
+        }
+        this.selectRow(row);
         break;
-		}
-	},
+      case 13:
+        this.sendAction('enter');
+        break;
+    }
+  },
 
   getSelectedRow: function() {
     return this.$().find('tr.selected:first');
-	},
+  },
 
   selectRow: function(row) {
     var selected = this.getSelectedRow();
@@ -225,14 +237,13 @@ export default Ember.Component.extend(Ember.Evented, {
       if (selected) {
         selected.addClass('selected');
         var selectedData = this.$().DataTable().row(row).data();
-				
-				this.sendAction('select', selectedData);
-			}
-		}		
-	},
+        
+        this.sendAction('select', selectedData);
+      }
+    }   
+  },
 
   onResize: function() {
     // this.redraw(false);
   }
 });
-
